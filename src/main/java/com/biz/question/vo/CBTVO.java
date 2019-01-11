367555c2effcadab6b0ab38ab0040efdd9b285da
eunhyanggo <hyang2lol@naver.com>
2019-01-11 17:00:55 +0900

    first

---
+++ src/main/java/com/biz/question/vo/CBTVO.java
@@ -0,0 +1,18 @@
+package com.biz.question.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CBTVO {
+
+	
+	  private String cbt_question;
+	  private String cbt_num1 ;
+	  private String cbt_num2 ;
+	  private String  cbt_num3 ;
+	  private String  cbt_num4 ;
+}
